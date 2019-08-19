1c6a5bf24fdd8c0a92a2c742578beaf292fe9ac9
Stefankar1000 <stefankar1000@gmail.com>
2019-08-19 02:06:57 +0200

    Fish: Tidy up the fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
-export PATH="$PATH:$HOME/bin"
+#Env variables
+export GOROOT="$HOME/dev/go"
+export TERM="st"
+export EDITOR="vim"
 export DOTNET_CLI_TELEMETRY_OPTOUT="true"
+export PATH="$PATH:$HOME/bin:$GOROOT/bin"
 
-# Set vi key bindings
+# Set vi-hybrid (combined vi with C-x binds) key bindings
 # fish_hybrid_key_bindings
 
 # Set the prompt
@@ -12,17 +16,19 @@ function fish_prompt
     echo -n " "
 end
 
-#Env variables
-export TERM="st"
-export EDITOR="vim"
-
+# Common quality of life aliases
 abbr _ "sudo"
+alias please="sudo"
+alias pls="sudo"
 abbr nuke "killall -9"
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 
+# Aliases for frequently used programs and scripts
+alias termbin="nc termbin.com 9999"
 alias ls="lsd"
+alias dotfiles="git --git-dir ~/.dotfiles --work-tree ~"
 abbr md "mkdir"
 abbr vi "$EDITOR"
 abbr svi "sudo $EDITOR"
@@ -34,20 +40,17 @@ abbr clip "xclip -selection clipboard"
 abbr vps "ssh predator@51.68.142.188"
 abbr ytdwn "youtube-dl -x --embed-thumbnail --audio-quality 6 --audio-format mp3"
 abbr record "ffmpeg -f x11grab -s 1440x900 -i :0.0 -r 30 -vcodec libx264 recording.mkv"
-abbr git-dot "git --git-dir ~/.dotfiles --work-tree ~"
-alias gpuwatch="python3.7 -m gpustat -i -c"
 
+# Shorthand abbreviantions for commonly used directories
 abbr bin "cd ~/bin"
 abbr dev "cd ~/dev"
 abbr doc "cd ~/doc"
 abbr dot "cd ~/dot"
 abbr dwn "cd ~/dwn"
 abbr img "cd ~/img"
+abbr msc "cd ~/msc"
 abbr snd "cd ~/snd"
+abbr src "cd ~/src"
 abbr tmp "cd ~/tmp"
 abbr vid "cd ~/vid"
-abbr src "cd ~/src"
-abbr msc "cd ~/msc"
-abbr games "cd ~/.games"
-
-abbr osudir "cd ~/.local/share/wineprefixes/osu/drive_c/users/stefan/Local\ Settings/Application\ Data/osu!/"
+alias osudir="cd ~/.local/share/wineprefixes/osu/drive_c/users/stefan/Local\ Settings/Application\ Data/osu!/"
